275ec737cf72dc5360a0bd5fa33511540a5376cd
Cody Harmon <codyharmon@silvercodex.fios-router.home>
2020-04-24 12:40:34 -0500

    updates

--- parse.go
+++ parse.go
@@ -21,10 +21,24 @@ func Parse(r io.Reader) ([]Link, error) {
 		return nil, err
 	}
 	nodes := linkNodes(doc)
+	var links []Link
+
 	for _, node := range nodes {
-		fmt.Println(node)
+		links = append(links, buildLink(node))
 	}
-	return nil, nil
+	return links, nil
+}
+
+func buildLink(n *html.Node) Link {
+	var ret Link
+	for _, attr := range n.Attr {
+		if attr.Key == "href" {
+			ret.Href = attr.Val
+			break
+		}
+	}
+	ret.Text = "TODO: Parse the Text..."
+	return ret
 }
 
 func linkNodes(n *html.Node) []*html.Node {
