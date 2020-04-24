6c7338623e72f746f2cabf856865dc2d5b072c62
Cody Harmon <codyharmon@silvercodex.fios-router.home>
2020-04-24 11:18:18 -0500

    test

---
+++ parse.go
@@ -0,0 +1,13 @@
+package link
+
+//link represents a link in a HTML document 
+type Link struct{
+	Href string,
+	Text string,
+}
+
+//Parse will take an HTML document and return slice of links parsed from it 
+func Parse(r io.Reader) ([]Link, error) {
+	return nil, nil
+}
+
