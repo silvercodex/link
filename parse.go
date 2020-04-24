08e878440fabe74ea7e4163ba4a3eb268a29bf10
Cody Harmon <codyharmon@silvercodex.fios-router.home>
2020-04-24 11:52:25 -0500

    updates

--- parse.go
+++ parse.go
@@ -1,6 +1,11 @@
 package link
 
-import "io"
+import (
+	"fmt"
+	"io"
+
+	"golang.org/x/net/html"
+)
 
 //link represents a link in a HTML document
 type Link struct {
@@ -10,5 +15,18 @@ type Link struct {
 
 //Parse will take an HTML document and return slice of links parsed from it
 func Parse(r io.Reader) ([]Link, error) {
+
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	dfs(doc)
 	return nil, nil
+}
+
+func dfs(n *html.Node, padding string) {
+	fmt.Println(padding, n.Data)
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		dfs(c, padding+"  ")
+	}
 }
